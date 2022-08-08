c01b0228fd594dad53a13171fc8d5d1a4598bd8a
Gabriel Dumitru <contactgabytm@gmail.com>
2022-08-08 13:26:56 +0300

    [api] add tuple and pair

---
+++ api/src/main/java/me/gabytm/minecraft/arcaneshop/api/util/collection/Pair.java
@@ -0,0 +1,28 @@
+package me.gabytm.minecraft.arcaneshop.api.util.collection;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Pair<A, B> {
+
+    private final A first;
+    private final B second;
+
+    public static @NotNull <A, B> Pair<A, B> of(@Nullable final A first, @Nullable final B second) {
+        return new Pair<>(first, second);
+    }
+
+    Pair(@Nullable final A first, @Nullable final B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A first() {
+        return first;
+    }
+
+    public B second() {
+        return second;
+    }
+
+}
---
+++ api/src/main/java/me/gabytm/minecraft/arcaneshop/api/util/collection/Tuple.java
@@ -0,0 +1,25 @@
+package me.gabytm.minecraft.arcaneshop.api.util.collection;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class Tuple<A, B, C> extends Pair<A, B> {
+    private final C third;
+
+    public static @NotNull <A, B, C> Tuple<A, B, C> of(
+            @Nullable final A first, @Nullable final B second,
+            @Nullable final C third
+    ) {
+        return new Tuple<>(first, second, third);
+    }
+
+    Tuple(@Nullable final A first, @Nullable final B second, @Nullable final C third) {
+        super(first, second);
+        this.third = third;
+    }
+
+    public C third() {
+        return third;
+    }
+
+}
