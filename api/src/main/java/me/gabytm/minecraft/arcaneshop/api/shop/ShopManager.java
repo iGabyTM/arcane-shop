897401b187f1180e274713c27452917c5f32a055
Gabriel Dumitru <contactgabytm@gmail.com>
2022-10-24 13:52:06 +0300

    [api] add `ShopManager#buyItem` and `ShopManager#sellItem`

--- api/src/main/java/me/gabytm/minecraft/arcaneshop/api/shop/ShopManager.java
+++ api/src/main/java/me/gabytm/minecraft/arcaneshop/api/shop/ShopManager.java
@@ -1,5 +1,6 @@
 package me.gabytm.minecraft.arcaneshop.api.shop;
 
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,5 +11,9 @@ public interface ShopManager {
     @Nullable Shop getShop(@NotNull final String name);
 
     @NotNull Map<String, Shop> getShops();
+
+    boolean buyItem(@NotNull final Shop shop, @NotNull final ShopItem item, final int amount, @NotNull final Player player);
+
+    boolean sellItem(@NotNull final Shop shop, @NotNull final ShopItem item, final int amount, @NotNull final Player player);
 
 }
